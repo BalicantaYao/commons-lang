c11c9daca208ccb77b337e7f1fbb52b15473737f
Balicanta Yao <balicanta.yao@gmail.com>
2014-10-21 09:59:52 +0800

    Add isSameYear Method

--- src/main/java/org/apache/commons/lang3/time/DateUtils.java
+++ src/main/java/org/apache/commons/lang3/time/DateUtils.java
@@ -197,6 +197,54 @@ public class DateUtils {
 
     //-----------------------------------------------------------------------
     /**
+     * <p>Checks if two date objects are on the same Year ignoring month, date and time.</p>
+     *
+     * <p>
+     * 28 Mar 2002 13:45 and 28 Mar 2002 06:01 would return true.
+     * 28 Mar 2002 13:45 and 12 Mar 2003 13:45 would return false.
+     * </p>
+     *
+     * @param date1  the first date, not altered, not null
+     * @param date2  the second date, not altered, not null
+     * @return true if they represent the same year
+     * @throws IllegalArgumentException if either date is <code>null</code>
+     */
+    public static boolean isSameYear(final Date date1, final Date date2) {
+
+        if (date1 == null || date2 == null) {
+            throw new IllegalArgumentException("The date must not be null");
+        }
+
+        final Calendar cal1 = Calendar.getInstance();
+        cal1.setTime(date1);
+        final Calendar cal2 = Calendar.getInstance();
+        cal2.setTime(date2);
+        return isSameYear(cal1, cal2);
+    }
+
+    /**
+     * <p>Checks if two calendar objects are on the same year ignoring month, day.</p>
+     *
+     * <p>
+     * 28 Mar 2002 13:45 and 28 Mar 2002 06:01 would return true.
+     * 28 Mar 2002 13:45 and 12 Mar 2003 13:45 would return false.
+     * </p>
+     *
+     * @param cal1  the first calendar, not altered, not null
+     * @param cal2  the second calendar, not altered, not null
+     * @return true if they represent the same day
+     * @throws IllegalArgumentException if either calendar is <code>null</code>
+     */
+    public static boolean isSameYear(final Calendar cal1, final Calendar cal2) {
+        if (cal1 == null || cal2 == null) {
+            throw new IllegalArgumentException("The date must not be null");
+        }
+        return (cal1.get(Calendar.ERA) == cal2.get(Calendar.ERA) &&
+                cal1.get(Calendar.YEAR) == cal2.get(Calendar.YEAR));
+    }
+
+    //-----------------------------------------------------------------------
+    /**
      * <p>Checks if two date objects represent the same instant in time.</p>
      *
      * <p>This method compares the long millisecond time of the two objects.</p>
